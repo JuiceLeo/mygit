d14906348a19512054f563dbdb91b2c08e07daa1
lfy <412799071@qq.com>
2019-03-10 09:38:18 +0800

    add a c++ file

---
+++ first.cpp
@@ -0,0 +1,15 @@
+/*********************************************************
+	> File Name:first.cpp
+	> Author: 
+	>Mail: 
+	> Created Time:2019年03月10日 星期日 09时36分00秒
+*****************************************************/
+ 
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+ 	cout << "hello c++" << endl;
+}
